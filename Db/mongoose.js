b6e8ab91e77e53f3448066735385f90956ada343
ankitkrks1 <ankit.kr.ks@gmail.com>
2022-03-25 12:44:09 +0530

    2

--- Db/mongoose.js
+++ Db/mongoose.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
 
-const url = "mongodb://localhost:27017/newTaskApp"
+const url = process.env.MONGODB_URL
+
+// "mongodb://localhost:27017/newTaskApp"
 mongoose.connect(url)
+
